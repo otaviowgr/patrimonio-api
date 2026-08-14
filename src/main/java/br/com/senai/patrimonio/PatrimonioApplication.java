c9545d63c2ccd1813a0542e55292c533a2692820
OTÁVIO WAGNER <otavio_wagner@estudante.sesisenai.org.br>
2026-08-13 21:49:52 -0300

    Metodos

--- src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
+++ src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
@@ -1,6 +1,7 @@
 package br.com.senai.patrimonio;
 
 import br.com.senai.patrimonio.model.Empresa;
+import br.com.senai.patrimonio.model.Endereco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +13,26 @@ public class PatrimonioApplication {
 
 		Empresa empresa = new Empresa();
 		empresa.setRazaoSocial("Senai LTDA");
-		System.out.print(empresa.getRazaoSocial());
+		System.out.println(empresa.getRazaoSocial());
+
+		Endereco endereco = new Endereco();
+		endereco.setRua("Rua General Lauro Sodré");
+		System.out.println(endereco.getRua());
+
+		endereco.setNumero("Nº 300");
+		System.out.println(endereco.getNumero());
+
+		endereco.setComplemento("Sem Complemento");
+		System.out.println(endereco.getComplemento());
+
+		endereco.setBairro("Comerciário");
+		System.out.println(endereco.getBairro());
+
+		endereco.setCidade("Criciúma");
+		System.out.println(endereco.getCidade());
+
+		endereco.setEstado("Santa Catarina");
+		System.out.println(endereco.getEstado());
 	}
 
 }
